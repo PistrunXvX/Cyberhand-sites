c31604d7f6b469461968c1aa8f73cef7fe7531a5
ferismer <54484370+ferismer@users.noreply.github.com>
2020-09-03 16:38:13 +0300

    Я удалил global.scss

--- src/App.svelte
+++ src/App.svelte
@@ -8,16 +8,15 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
 </svelte:head> -->
-<canvas class="container-fluid">
+   <div class="container-fluid">
 	<Header/>
 	<Main/>
 	<Footer/>
-</canvas>
+</div>
+
 
 
 <style lang="scss">
-	canvas{
-		background-color: #1D002B;
-		width:100%;
-	}
-</style>
+	@import 'style/theme.scss';
+	
+	
