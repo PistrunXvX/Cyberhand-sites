966e06e9be06dd5481e6b8d5b171c16ace4b81d9
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2020-09-15 01:20:53 +0300

    Add portfolio images

--- src/Components/Portfolio.svelte
+++ src/Components/Portfolio.svelte
@@ -17,7 +17,7 @@
         </div>
         <div class="row">
             <div class="col">
-                <div class="accordion" id="accordionExample">
+                <div class="accordion first__lend" id="accordionExample">
                     <div class="card">
                       <div class="card-header" id="headingOne">
                           <h2>
@@ -41,13 +41,13 @@
                                 </ol>
                                 <div class="carousel-inner">
                                   <div class="carousel-item active">
-                                    <img src="/img/first_land_1.jpg" class="d-block w-100" alt="...">
+                                    <img src="/img/first_lend_1.jpg" class="d-block w-100" alt="...">
                                   </div>
                                   <div class="carousel-item">
-                                    <img src="/img/first_land_2.jpg" class="d-block w-100" alt="...">
+                                    <img src="/img/first_lend_2.jpg" class="d-block w-100" alt="...">
                                   </div>
                                   <div class="carousel-item">
-                                    <img src="/img/first_land_3.jpg" class="d-block w-100" alt="...">
+                                    <img src="/img/first_lend_3.jpg" class="d-block w-100" alt="...">
                                   </div>
                                 </div>
                                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -63,6 +63,144 @@
                       </div>
                     </div>
             </div>
+            <div class="accordion second__lend" id="accordionExample">
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                      <h2>
+                         Квартирный вопрос
+                      </h2>
+                      <p>
+                        Веб дизайн для сайта продаж квартир.
+                      </p>
+                      <button class="btn btn-link btn-block text-right float-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                        Подробнее
+                      </button>
+                  </div>
+              
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div id="carouselExampleIndicators_2" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleIndicators_2" data-slide-to="0" class="active"></li>
+                              <li data-target="#carouselExampleIndicators_2" data-slide-to="1"></li>
+                              <li data-target="#carouselExampleIndicators_2" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img src="/img/second_lend_1.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/second_lend_2.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/second_lend_3.jpg" class="d-block w-100" alt="...">
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators_2" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators_2" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="accordion three__lend" id="accordionExample">
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                      <h2>
+                         Космическая одиссея
+                      </h2>
+                      <p>
+                        Веб дизайн для сайта организации праздников.
+                      </p>
+                      <button class="btn btn-link btn-block text-right float-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+                        Подробнее
+                      </button>
+                  </div>
+              
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div id="carouselExampleIndicators_3" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleIndicators_3" data-slide-to="0" class="active"></li>
+                              <li data-target="#carouselExampleIndicators_3" data-slide-to="1"></li>
+                              <li data-target="#carouselExampleIndicators_3" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img src="/img/three_lend_1.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/three_lend_2.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/three_lend_3.jpg" class="d-block w-100" alt="...">
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators_3" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators_3" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            <div class="accordion four__lend" id="accordionExample">
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                      <h2>
+                         Кондитерская
+                      </h2>
+                      <p>
+                        Веб дизайн для кондитерской.
+                      </p>
+                      <button class="btn btn-link btn-block text-right float-left" type="button" data-toggle="collapse" data-target="#collapseFoure" aria-expanded="true" aria-controls="collapseFoure">
+                        Подробнее
+                      </button>
+                  </div>
+              
+                  <div id="collapseFoure" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div id="carouselExampleIndicators_4" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                              <li data-target="#carouselExampleIndicators_4" data-slide-to="0" class="active"></li>
+                              <li data-target="#carouselExampleIndicators_4" data-slide-to="1"></li>
+                              <li data-target="#carouselExampleIndicators_4" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img src="/img/four_lend_1.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/four_lend_2.jpg" class="d-block w-100" alt="...">
+                              </div>
+                              <div class="carousel-item">
+                                <img src="/img/four_lend_3.jpg" class="d-block w-100" alt="...">
+                              </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators_4" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators_4" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Next</span>
+                            </a>
+                          </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
@@ -75,9 +213,20 @@
         background-color: $darkColor;
     }
 
+    .card{
+        background-color: $darkColor;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: 50% 50%;
+    }
+
+    .accordion{
+        padding-top: 10%;
+    }
+
     .portfolio__block{
 
-        padding-top: 10%;
+        padding-top: 15%;
 
         h1{
             color: $colorWhite;
@@ -88,8 +237,34 @@
         }
     } 
 
-    .card-header h2{
-        color: $colorWhite;
+    .card-header{
+
+        h2{
+            color: $colorWhite;
+        }
+        p{
+            color: $colorWhite;
+        }
+    }
+
+    .first__lend .card{
+        background-image: url("/img/first_lend_main.jpg");
+
+    }
+
+    .second__lend .card{
+        background-image: url("/img/second_lend_main.jpg");
+
+    }
+
+    .three__lend .card{
+        background-image: url("/img/three_lend_main.jpg");
+
+    }
+
+    .four__lend .card{
+        background-image: url("/img/four_lend_main.jpg");
+
     }
 
 </style>
