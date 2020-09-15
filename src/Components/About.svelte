ce5f1b17af4bebbd9a3be86bbf7c2d2b60228465
ferismer <54484370+ferismer@users.noreply.github.com>
2020-09-16 02:13:27 +0300

    about

--- src/Components/About.svelte
+++ src/Components/About.svelte
@@ -1,114 +1,201 @@
 <script>
 
 </script>
+<div class="container-fluid">
+    <section class="container-fluid first__container">
+        <div class="row">
+            <h1>О нас</h1>
+            <div class="col-xl-12">
+                <div class="about_block">
 
-<section class="container-fluid first__container">
-    <div class="row">
-        <h2>О нас</h2>
-        <div class="col-xl-12">
-            <div class="about_block">
+                    <div class="about_slider">
 
-                <div class="container about_slider">
+                        <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active" data-interval="2000">
+                                    <img src="/img/loshariki.jpg" class="d-block w-100" alt="">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/loshariki.jpg" class="d-block w-100" alt="">
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleInterval" role="button"
+                                data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleInterval" role="button"
+                                data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
 
+                    </div>
                 </div>
+
             </div>
-
-        </div>
-    </div>
-
-</section>
-
-<section class="container second__container">
-
-    <div class="row">
-        <h3>Мы команда с одним интересом!</h3>
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <div class="card__item">
-                <div class="card__item_image">
-                    
-                    <!-- <a href="#"></a> -->
-                </div>
-                
-                <div class="card__item_text">
-                    <h3>Шершнёв Артём</h3>
-                    <p>Веб - дизайнер.</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-
-            <div class="card__item">
-                <div class="card__item_image">
-                    
-                    <!-- <a href="#"></a> -->
-                </div>
-                
-                <div class="card__item_text">
-                    <h3>Шершнёв Артём</h3>
-                    <p>Веб - дизайнер.</p>
-                </div>
-            </div>
-
         </div>
 
-        
+    </section>
 
+    <section class="container-fluid second__container ">
 
         <div class="row">
-
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="card__item">
-                    <div class="card__item_image">
+            <h3 class="second__title">Мы команда с одним интересом!</h3>
+            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                
+                    <div class="card__item ">
                         
-                        <!-- <a href="#"></a> -->
+                        <div class="card__item_image">
+
+                            <img src="/img/card.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="card__item_text">
+                            <h3>Шершнёв Артём</h3>
+                            <p>Веб - дизайнер.</p>
+                        </div>
                     </div>
-                    
-                    <div class="card__item_text">
-                        <h3>Шершнёв Артём</h3>
-                        <p>Веб - дизайнер.</p>
+
+                </div>
+
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+                    <div class="card__item">
+                        <div class="card__item_image">
+
+                            <img src="/img/card.jpg" alt="" class="img-fluid">
+                        </div>
+
+                        <div class="card__item_text">
+                            <h3>Шершнёв Артём</h3>
+                            <p>Веб - дизайнер.</p>
+                        </div>
                     </div>
+
+                </div>
+
+
+
+
+                <div class="row ">
+
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                        <div class="card__item">
+                            <div class="card__item_image">
+
+                                <img src="/img/card.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <div class="card__item_text">
+                                <h3>Шершнёв Артём</h3>
+                                <p>Веб - дизайнер.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+                        <div class="card__item">
+                            <div class="card__item_image">
+
+                                <img src="/img/card.jpg" alt="" class="img-fluid">
+                            </div>
+
+                            <div class="card__item_text">
+                                <h3>Шершнёв Артём</h3>
+                                <p>Веб - дизайнер.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
 
-            <div class="col-xl-6col-lg-6col-md-6 col-sm-12 col-xs-12">
 
-                <div class="card__item">
-                    <div class="card__item_image">
-                        
-                        <!-- <a href="#"></a> -->
-                    </div>
-                    
-                    <div class="card__item_text">
-                        <h3>Шершнёв Артём</h3>
-                        <p>Веб - дизайнер.</p>
-                    </div>
-                </div>
+    </section>
 
-            </div>
-
-            
-
-        </div>
+</div>
+<style lang="scss">
+    @import "style/theme.scss";
+    @import "style/resize.scss";
 
 
-</section>
 
 
-<style>
-    @import "./src/style/theme.scss";
-    @import "./src/style/resize.scss";
 
-    .card__item_image{
+    .first__container {
+        padding-top: 10%;
 
-        background-image: url("/img/card.jpg");
-            background-size:  contain;
-            background-repeat: no-repeat;
-             width: 430px;
-             height: 350px;
-            
+
+        
+        
+        h1 {
+            text-transform: uppercase;
+            font-weight: 700;
+            padding-left: 6%;
+            font-size: 153px;
+            padding-bottom: 6%;
+            line-height: 115.3%
+        }
     }
+
+    .container-fluid {
+        padding-left: 3%;
+        padding-right: 3%;
+        background-color: $darkColor;
+        color: $colorWhite;
+        font-family: $opensans;
+    }
+
+
+    .second__container {
+        padding-bottom: 15%;
+        .card__item_text{
+            padding-top: 2%;
+            
+        
+
+        }
+
+        .second__title {
+            
+            text-align: center;
+            padding-top: 5%;
+            padding-bottom: 5%;
+            font-size: 46px;
+            line-height: 115.3%;
+        }
+
+        h3{
+            font-size: 46px;
+            font-weight: 700;
+            line-height: 115.3%;
+        }
+
+        p{
+            font-style: italic;
+            font-weight: 100;
+            font-size: 34px;
+        }
+       
+
+    .card__item{
+        padding-bottom: 20%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        
+    }
+
+}
+          
+    
+
 
 </style>
