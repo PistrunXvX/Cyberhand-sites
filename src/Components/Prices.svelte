efe94b26772280f0c56283bd87ba9ba261a1e3ce
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2020-09-15 21:38:54 +0300

    Add up animation

--- src/Components/Prices.svelte
+++ src/Components/Prices.svelte
@@ -3,7 +3,13 @@
 </script>
 
 <div class="container">
-    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                
+            </div>
+        </div>
+    </div>
 </div>
 <style>
     @import "../style/theme.scss";
